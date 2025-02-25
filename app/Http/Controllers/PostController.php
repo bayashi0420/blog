2a9941efc2485df45d7a795a749724d808428879
bayashi0420 <nobuhiro123456789@gmail.com>
2025-02-25 10:45:17 +0000

    etc

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use app\models\Post;
+
+
 
 class PostController extends Controller
 {
+    public function index(Post $post)
+    {
+        return $post->get();
+    }
     //
 }
